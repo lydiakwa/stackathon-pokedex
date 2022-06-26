5b946f2e2c4cd585094a2169a1805b77bb59f1ab
Lydia Kwa <lydiakwa@gmail.com>
2022-06-25 20:15:22 -0400

    Installed React Navigation react native's version of routes I think, still testing if I can render different pages

--- App.js
+++ App.js
@@ -2,25 +2,31 @@ import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
 import { QueryClientProvider, QueryClient } from 'react-query';
 import { ReactQueryDevtools } from 'react-query/devtools';
+import { NavigationContainer } from '@react-navigation/native';
+import { createNativeStackNavigator } from '@react-navigation/native-stack';
 
 import PokemonList from './Components/PokemonList';
+import HomeScreen from './Components/HomeScreen';
 
 const queryClient = new QueryClient();
+
+const Stack = createNativeStackNavigator();
 
 export default function App() {
   return (
     //this is like PROVIDER from react-redux
-    <NavigationContainer>
-      <QueryClientProvider client={queryClient}>
-        <View style={styles.container}>
-          <Text>Pokedex</Text>
-          <PokemonList />
-
-          <StatusBar style="auto" />
-          {/* <ReactQueryDevtools initialIsOpen={false} /> */}
-        </View>
-      </QueryClientProvider>
-    </NavigationContainer>
+    <QueryClientProvider client={queryClient}>
+      <NavigationContainer>
+        <Stack.Navigator initialRouteName="Home">
+          <Stack.Screen
+            name="Home"
+            component={HomeScreen}
+            options={{ title: 'Welcome' }}
+          />
+          <Stack.Screen name="List" component={PokemonList} />
+        </Stack.Navigator>
+      </NavigationContainer>
+    </QueryClientProvider>
   );
 }
 
---
+++ Components/HomeScreen.js
@@ -0,0 +1,11 @@
+import { Text, View } from 'react-native';
+
+const HomeScreen = ({ navigation }) => {
+  return (
+    <View>
+      <Text>HI</Text>
+    </View>
+  );
+};
+
+export default HomeScreen;
